116b0a97e46be02efc35cfa40149e71f64fad844
JakubS <myGitHub4publicprojects@users.noreply.github.com>
2015-04-13 21:18:25 +0100

    generateAndInsertSMA was removed, instead two function were created to generate SMAs and create table containg them

--- dbcreationfunctions.py
+++ dbcreationfunctions.py
@@ -27,26 +27,70 @@ def fromTexttoDB(directory):
             c = conn.cursor()
             c.execute("CREATE TABLE {table_name}(ID INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL)".format(table_name = table_name))
             c.executemany("INSERT INTO {table_name} (ID, Date, Open, High, Low, Close, Volume) VALUES (?, ?, ?, ?, ?, ?, ?)".format(table_name = table_name), data[1:])      
-
-def generateAndInsertSMA(table_name, SMA_period):
-    '''table_name: string;
-    SMA_period: int
-    generates SMA of a given period for a given stock (table_name) and populate
-    the table with SMA values,
-    if a clumn where data is to be inserted does not existed, function creates it'''
-    # check if the column exists, if not, create it
-    c.execute("PRAGMA table_info({table})".format(table = 'atc'))
-    if 'SMA{period}'.format(period = SMA_period) not in [e[1] for e in c.fetchall()]:
-        c.execute("ALTER TABLE {table} ADD COLUMN SMA{period} REAL".format(table = table_name, period = SMA_period))
-        
-    c.execute("SELECT Date, Close FROM {table}".format(table = table_name))
-    date_close = c.fetchall()
-    for e in range(SMA_period -1, len(date_close)):
+      
+def generateSMA(SMA_period, table_name):
+    ''' SMA_period: integer,
+    table_name: string, name of a table in db.
+    return a list of SMA values, the list lenght is equal to numer of rows in a table
+    in the first rows where SMA cannot be calculated None is inserted
+    '''
+    c.execute("SELECT Close FROM {table}".format(table = table_name))
+    price_at_close = c.fetchall()
+    
+    close_list = []
+    
+    for e in range(SMA_period -1, len(price_at_close)):
         sum_close = 0
         counter = SMA_period - 1
         for i in range(SMA_period):
-            sum_close += date_close[e - counter][1]
+            sum_close += price_at_close[e - counter][0]
             counter -= 1
         sma = sum_close/SMA_period
-        c.execute("UPDATE {table} SET SMA{SMA}={sma} WHERE Date={date}".format(table = table_name, SMA = SMA_period, sma = sma, date = date_close[e][0]))
-        conn.commit()
+        close_list.append(sma)
+    close_list = (SMA_period - 1) * [None] + close_list
+    return close_list
+    
+def tablesWithDataandSMA(table_name):
+    ''' generates table with following fields:
+        ID INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL
+        SMA 2 to 20, from 20 to 100 every 5, from 100 to 200 every 10, 200, 250 and 300
+    '''
+    # list of SMAs for w given stock
+# change next line
+    list_of_smas = range(2, 10) # + range(20, 100, 5) + range(100, 200, 10) + [200, 250, 300]
+    
+    #append each list of SMA values to list_of_smas_values
+    list_of_smas_values = []
+    for e in list_of_smas:
+        list_of_smas_values.append(generateSMA(e, table_name))
+        
+    # list of lists of SMAs for each day
+    smas_by_days = []
+    for e in range(len(list_of_smas_values[0])):
+        same_day_smas = []
+        for i in range(len(list_of_smas_values)):
+            same_day_smas.append(list_of_smas_values[i][e])
+        smas_by_days.append(same_day_smas)
+        
+    # get data from table_name in a form of list of lists (individual records)
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM {table}".format(table = table_name))
+        data = [list(e) for e in c.fetchall()]
+     
+    # append SMAs at the end of each record in data
+    extended_data = []
+    for e in range(len(data)):
+        extended_data.append(data[e] + smas_by_days[e])
+           
+    # create new table -> 'table_name + withSMAs' and inserts data from table_name
+    # and SMAs (extended_data)
+    smas = ','.join([' SMA' + str(e) + ' REAL' for e in list_of_smas])
+    smas2 = ','.join([' SMA' + str(e) for e in list_of_smas])
+    questmarks = '?' + (', ?') * (6 + len(list_of_smas))
+    with conn:
+        c = conn.cursor()
+        c.execute("CREATE TABLE {new_table} (ID INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL, {smas})".format(new_table = table_name + 'withSMAs', smas = smas))
+        c.executemany("INSERT INTO {new_table} (ID, Date, Open, High, Low, Close, Volume, {smas2}) VALUES ({questmarks})".format(new_table = table_name + 'withSMAs', smas2 = smas2, questmarks = questmarks), extended_data)
+
+ 
