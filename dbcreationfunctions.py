7fc54ed2707f0609cfbcfa216a9ddd7e9b17ebc9
JakubS <myGitHub4publicprojects@users.noreply.github.com>
2015-04-13 08:22:22 +0100

    fromTexttoDB function code was simplified, tableCreate function was removed

--- dbcreationfunctions.py
+++ dbcreationfunctions.py
@@ -3,43 +3,17 @@ import sqlite3
 conn = sqlite3.connect('/home/jakub/Documents/analizy-finansowe/relative-strength/GPWstocks.db')
 conn.text_factory = str
 c = conn.cursor()
-    
-def tableCreate(name):
-    ''' name: str
-    creates table in a database'''
-    c.execute("CREATE TABLE {0}(ID INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL)".format(name))
-    
+  
 def fromTexttoDB(directory):
     '''directory: string;
     from each text file in a directory extracts data and saves it in a database'''
     import os.path
-    list_of_files = os.listdir(directory)
-    last_started = None
-    
-    #   removes table that was started in previous run and might not be filled completely
-    for filee in list_of_files:
-        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", ('table' + filee[:3],))
-        fetched = c.fetchone()
-        if fetched != None:
-            last_started = fetched[0] + '.txt'
-        if fetched == None:
-            if last_started != None:
-                c.execute("DROP TABLE {table_name}".format(table_name = last_started[:-4]))
-            break
-    
-    # create and fill tables starting with first file or one that corresponds to
-    # the table that was removed by previuos block                
-    if last_started == None:
-        last_started = 'table' + list_of_files[0]  
-              
-    for e in range(list_of_files.index(last_started[5:]), len(list_of_files)):
-        
-        table_name = 'table' + list_of_files[e][:3]    
-        tableCreate(table_name)
-        print list_of_files[e]
-    
+    # list of files in a directory without temp files:
+    list_of_files = [e for e in os.listdir(directory) if not e.endswith('~')]
+       
+    for e in range(len(list_of_files)):        
         import csv        
-        with open('/home/jakub/Documents/analizy-finansowe/wse stocks/{filee}'.format(filee = list_of_files[e]), 'rb') as input_file:
+        with open('{dir}/{filee}'.format(dir = directory, filee = list_of_files[e]), 'rb') as input_file:
             reader = csv.reader(input_file)
             data = []
             ID = 0
@@ -47,9 +21,12 @@ def fromTexttoDB(directory):
                 row.insert(0, ID)
                 ID += 1
                 data.append(row[:-1])
-        c.executemany("INSERT INTO {table_name} (ID, Date, Open, High, Low, Close, Volume) VALUES (?, ?, ?, ?, ?, ?, ?)".format(table_name = 'table' + list_of_files[e][:3]), data[1:])
-        conn.commit()
-            
+                
+        table_name = 'table' + list_of_files[e][:3]   
+        with conn:
+            c.execute("CREATE TABLE {table_name}(ID INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL)".format(table_name = table_name))
+            c.executemany("INSERT INTO {table_name} (ID, Date, Open, High, Low, Close, Volume) VALUES (?, ?, ?, ?, ?, ?, ?)".format(table_name = table_name), data[1:])      
+
 def generateAndInsertSMA(table_name, SMA_period):
     '''table_name: string;
     SMA_period: int
